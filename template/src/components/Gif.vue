9d36577bcebf1a86ae85f2312d7fbed002573d43
hessnd <n.hess@bigspaceship.com>
2016-10-27 13:37:07 -0400

    fixed es lint errors

--- template/src/components/Gif.vue
+++ template/src/components/Gif.vue
@@ -21,7 +21,7 @@ export default {
     this.fetchGif(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
   },
   methods: {
-    fetchGif () {
+    fetchGif() {
       this.$http.jsonp('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=pg-13').then((response) => {
         this.gif = response.body.data.image_url{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
       }, (response) => {
